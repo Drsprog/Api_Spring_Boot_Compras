b76e0a22108f3055bec873b320dffc815939194e
David Ricapa Sanchez <albertxd40@gmail.com>
2026-01-28 17:21:21 -0500

    Primer commit de la API

---
+++ src/main/java/com/drsprog/compras_api/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.drsprog.compras_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Entidad que representa un usuario del sistema.
+ */
+@Entity
+@Table(name = "USUARIO")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO")
+    private Long id;
+
+    @Column(name = "NOMBRE", nullable = false)
+    private String nombre;
+
+    @Column(name = "APELLIDO", nullable = false)
+    private String apellido;
+
+    @Column(name = "CONTRASENA", nullable = false)
+    private String contrasena;
+
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "DNI", nullable = false, unique = true)
+    private String dni;
+
+    @Column(name = "FECHA_NACIMIENTO", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Column(name = "TELEFONO")
+    private String telefono;
+
+    @Column(name = "FECHA_CREACION", nullable = false)
+    @Builder.Default
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+}
